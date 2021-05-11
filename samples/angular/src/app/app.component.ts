0348a73976867928805b4d2e66798cb676aef502
Mohammad Rahhal <m.r992@hotmail.com>
2021-05-11 16:50:00 +0300

    Update angular sample

--- samples/angular/src/app/app.component.ts
+++ samples/angular/src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+  values: number[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.values.push(i);
+    }
+  }
+
+  ngDoCheck() {
+    // To test when change detection is being triggered
+    console.log('ngDoCheck');
+  }
+
+  _onTopReached() {
+    console.log('_onTopReached');
+  }
+
+  _onScrolled() {
+    console.log('_onScrolled');
+
+    // This event doesn't run inside angular on purpose for performance.
+    // To trigger change detection you should call `zone.run()`.
+  }
 }
