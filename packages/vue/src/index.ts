c669b513aedc7bb9f988b18bc6621383a8bbb2a9
Mohammad Rahhal <m.r992@hotmail.com>
2021-05-24 11:19:46 +0300

    Properly produce declaration files for vue package

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -2,10 +2,8 @@ import { App } from 'vue';
 
 import ScrollComponent from './scroll.vue';
 
-const install = (app: App) => {
-  app.component(ScrollComponent.name, ScrollComponent);
+(ScrollComponent as any).install = (app: App) => {
+  app.component('mr-scroll', ScrollComponent);
 };
-
-(ScrollComponent as any).install = install;
 
 export default ScrollComponent;
